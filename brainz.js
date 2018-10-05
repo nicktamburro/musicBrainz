aefe3517abd148e7f668dead3c8e10129e09fc4d
nicktamburro <nicholastamburro@gmail.com>
2018-10-04 21:45:27 -0700

    messing around

--- brainz.js
+++ brainz.js
@@ -1,17 +1,25 @@
 const mb = require('musicbrainz');
 
-
-/*
+//LOOK UP RELEASE BY... what? ID?
+  
 mb.lookupRelease('283821f3-522a-45ca-a669-d74d0b4fb93a', ['artists'], function (error, release) {
     console.log(release);
-});*/
+});
 
-/*mb.searchArtists('The White Stripes', {}, function(err, artists){
+//SEARCH ARTIST
+
+mb.searchArtists('Nirvana', {}, function(err, artists){
     console.log(artists);
-});*/
-/*mb.searchRecordings('Seven Nation Army', { artist: 'The White Stripes' }, function(err, recordings){
+});
+
+//SEARCH RECORDING (= search song?) , and narrow it down inside the curly braces
+
+mb.searchRecordings('Amen, Brother', { artist: 'The Winstons' }, function(err, recordings){
     console.log(recordings);
-});*/
-mb.searchReleases('Elephant', { country: 'US', artist: 'The White Stripes' }, function(err, releases){
+});
+
+//SEARCH RELEASES
+//so the parameters to narrow it down go in the {}'s, we still have to pare down all the data we get
+*mb.searchReleases('Elephant', { country: 'US', artist: 'The White Stripes' }, function(err, releases){
     console.log(releases);
 });
