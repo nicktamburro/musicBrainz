e95a97c6c726f3550d7b33e9af5e198407009bf8
nicktamburro <nicholastamburro@gmail.com>
2018-11-19 20:03:12 -0800

    paring down data

--- brainz.js
+++ brainz.js
@@ -1,11 +1,14 @@
+//so biggest thing is we have to sort through this data, it's a huge set of objects within
+//objects, and we just have to pare it down
+
 const mb = require('musicbrainz');
 
 //LOOK UP RELEASE BY... what? ID?
 //and we need to get body-parser, and probably some json middleware in here...
   
-mb.lookupRelease('283821f3-522a-45ca-a669-d74d0b4fb93a', ['artists'], function (error, release) {
+/*mb.lookupRelease('283821f3-522a-45ca-a669-d74d0b4fb93a', ['artists'], function (error, release) {
     console.log(release);
-});
+});*/
 
 //SEARCH ARTIST
 
@@ -15,15 +18,15 @@ mb.searchArtists('Nirvana', {}, function(err, artists){
 
 //SEARCH RECORDING (= search song?) , and narrow it down inside the curly braces
 
-mb.searchRecordings('Amen, Brother', { artist: 'The Winstons' }, function(err, recordings){
-    console.log(recordings);
-});
+// mb.searchRecordings('Amen, Brother', { artist: 'The Winstons' }, function(err, recordings){
+//     console.log(recordings);
+// });
 
 //SEARCH RELEASES
 //so the parameters to narrow it down go in the {}'s, we still have to pare down all the data we get
-mb.searchReleases('Elephant', { country: 'US', artist: 'The White Stripes' }, function(err, releases){
-    console.log(releases);
-});
+// mb.searchReleases('Elephant', { country: 'US', artist: 'The White Stripes' }, function(err, releases){
+//     console.log(releases);
+// });
 
 //AND THEN, after we've established a musicBrainz ID, we pass it to acoustic brainz, to get
 //more data... like
